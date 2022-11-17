be982d541b84d08c6181ec0d9606d7f4585e2054
prjBTS <117942356+prjBTS@users.noreply.github.com>
2022-11-17 09:17:40 +0530

    updating src

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-
-import auth from './auth';
-import admin from './admin';
-import item from './item';
-import order from './order'
-
-export const reducers = combineReducers({ auth, admin, item, order });
