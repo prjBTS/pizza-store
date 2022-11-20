21cfe65f77ec281ec72639a54861fb7473138b93
prjBTS <117942356+prjBTS@users.noreply.github.com>
2022-11-20 02:46:05 -0500

    fixed bugs

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import { CircularProgress } from '@mui/material';
