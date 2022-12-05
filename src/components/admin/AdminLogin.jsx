7b321b91814b280ad31f2ab2f3b5d6c0e3b2f32e
prjBTS <117942356+prjBTS@users.noreply.github.com>
2022-12-05 11:46:52 -0500

    loop

--- src/components/admin/AdminLogin.jsx
+++ src/components/admin/AdminLogin.jsx
@@ -29,6 +29,7 @@ const initialState = {
 const AdminLogIn = () => {
     const is_admin = useSelector((state) => state.admin.adminData);
     const isUser = useSelector((state) => state.auth.authData);
+    console.log("AdminLogin:", is_admin.isUser);
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const [formData, setformData] = useState(initialState)
@@ -42,80 +43,80 @@ const AdminLogIn = () => {
         setformData({ ...formData, [e.target.name]: e.target.value });
     }
 
-    return
-    isUser ? (
-        <Navigate exact to="/profile" />
-    ) :
-        (is_admin ?
-            <Navigate exact to="/admin/dashboard" />
-
+    return (
+        isUser ?
+            <Navigate exact to="/profile" />
             :
+            is_admin ?
+                <Navigate exact to="/admin/dashboard" />
 
-            <ThemeProvider theme={theme}>
-                <Container component="main" maxWidth="xs">
-                    <CssBaseline />
-                    <Box
-                        sx={{
-                            marginTop: 8,
-                            display: 'flex',
-                            flexDirection: 'column',
-                            alignItems: 'center',
-                        }}
-                    >
-                        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                            <LockOutlinedIcon />
-                        </Avatar>
-                        <Typography component="h1" variant="h5">
-                            Admin Login
-                        </Typography>
-                        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-                            <TextField
-                                margin="normal"
-                                required
-                                fullWidth
-                                id="email"
-                                label="Email Address"
-                                name="email"
-                                autoComplete="email"
-                                onChange={handleChange}
-                                autoFocus
-                            />
-                            <TextField
-                                margin="normal"
-                                required
-                                fullWidth
-                                name="password"
-                                label="Password"
-                                type="password"
-                                id="password"
-                                onChange={handleChange}
-                                autoComplete="current-password"
-                            />
-                            <FormControlLabel
-                                control={<Checkbox value="remember" color="primary" />}
-                                label="Remember me"
-                            />
-                            <Button
-                                type="submit"
-                                fullWidth
-                                variant="contained"
-                                sx={{ mt: 3, mb: 2 }}
-                            >
-                                Sign In
-                            </Button>
-                            <Grid container>
-                                <Grid item xs>
-                                    <Link href="/" variant="body2">
-                                        Not Admin?
-                                    </Link>
+                :
+
+                <ThemeProvider theme={theme}>
+                    <Container component="main" maxWidth="xs">
+                        <CssBaseline />
+                        <Box
+                            sx={{
+                                marginTop: 8,
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'center',
+                            }}
+                        >
+                            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                                <LockOutlinedIcon />
+                            </Avatar>
+                            <Typography component="h1" variant="h5">
+                                Admin Login
+                            </Typography>
+                            <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                                <TextField
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    id="email"
+                                    label="Email Address"
+                                    name="email"
+                                    autoComplete="email"
+                                    onChange={handleChange}
+                                    autoFocus
+                                />
+                                <TextField
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    name="password"
+                                    label="Password"
+                                    type="password"
+                                    id="password"
+                                    onChange={handleChange}
+                                    autoComplete="current-password"
+                                />
+                                <FormControlLabel
+                                    control={<Checkbox value="remember" color="primary" />}
+                                    label="Remember me"
+                                />
+                                <Button
+                                    type="submit"
+                                    fullWidth
+                                    variant="contained"
+                                    sx={{ mt: 3, mb: 2 }}
+                                >
+                                    Sign In
+                                </Button>
+                                <Grid container>
+                                    <Grid item xs>
+                                        <Link href="/" variant="body2">
+                                            Not Admin?
+                                        </Link>
+                                    </Grid>
                                 </Grid>
-                            </Grid>
+                            </Box>
                         </Box>
-                    </Box>
-                </Container>
-            </ThemeProvider>
+                    </Container>
+                </ThemeProvider>
 
-        );
+    );
 }
 
 export default AdminLogIn
